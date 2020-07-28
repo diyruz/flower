4c7d6ccf424a931bb1d49ef6663c33bd5916e9f3
nur <nur.php@gmail.com>
2020-07-29 06:00:12 +0800

    Move code to zstack-lib

--- Source/battery.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BATTERY_H
-#define _BATTERY_H
-extern uint16 getBatteryVoltage(void);
-extern uint8 getBatteryVoltageZCL(void);
-extern uint8 getBatteryRemainingPercentageZCL(void);
-#endif
--- Source/hal/ds18b20.h
+++
@@ -1,5 +0,0 @@
-#ifndef ds18b20_h
-int16 readTemperature(void);
-#define ds18b20_h
-#endif
-
