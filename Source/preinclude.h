e61c167acecff6c491cbf00bb9a8605a76ac308b
nur <nur.php@gmail.com>
2021-04-21 22:14:35 +0800

    use uart output

--- Source/preinclude.h
+++ Source/preinclude.h
@@ -60,8 +60,8 @@
 #define POWER_SAVING
 // #define DO_DEBUG_UART
 #elif defined(HAL_BOARD_CHDTECH_DEV)
-// #define DO_DEBUG_UART
-#define DO_DEBUG_MT
+#define DO_DEBUG_UART
+// #define DO_DEBUG_MT
 
 #endif
 
